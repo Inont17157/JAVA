a7cabe7bf928176eafac3fe8e5f98d08a0d024d9
Inont17157 <vv0930587842@gmail.com>
2022-10-20 21:07:04 -0700

    แก้ไข
    แก้ไข

--- Basic/hello.java
+++ Basic/hello.java
@@ -1,5 +1,7 @@
 public class hello{
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        System.out.println("my name is INont");
+        System.out.println("55555555555");
     }
 }
